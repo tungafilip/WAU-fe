ea4fd478dae9674a4f99512720c09039faae10a4
Filip Tunguz <filiptunguz.su@gmail.com>
2021-10-15 15:34:51 +0200

    NavBar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './NavBar.scss';
+
+class NavBar extends Component {
+	render() {
+		return (
+			<nav>
+				<ul>
+					<li>Page #1</li>
+					<li>Page #2</li>
+					<li>Page #3</li>
+					<li>Page #4</li>
+				</ul>
+			</nav>
+		);
+	}
+}
+
+export default NavBar;

783bc7c1fe7321a84efb5de7fd4f7d615b27c073
Filip Tunguz <filiptunguz.su@gmail.com>
2021-10-18 12:56:26 +0200

    Symfony connection Test

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
 import './NavBar.scss';
+import axios from "axios";
 
 class NavBar extends Component {
+	constructor(props) {
+		super();
+
+		this.state = {
+			result: 'Click Me'
+		};
+
+		this.axiosTest = this.axiosTest.bind(this);
+	}
+
+	axiosTest() {
+		axios.get('http://localhost:8000/api/test')
+			.then(res => {
+				const test = res.data;
+				this.setState({result: test});
+			});
+	}
+
 	render() {
 		return (
 			<nav>
 				<ul>
-					<li>Page #1</li>
+					<li onClick={this.axiosTest}>{ this.state.result }</li>
 					<li>Page #2</li>
 					<li>Page #3</li>
 					<li>Page #4</li>
